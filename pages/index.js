22188f139d69693ab4ca94ab011d60c543d82e23
Paul Terry <paul.terry84@gmail.com>
2020-10-06 08:36:33 +0100

    Initial commit from Create Next App

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+export default function Index() {
+  return (
+    <>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+      </Head>
+      <div>Hello world!</div>
+    </>
+  );
+}
