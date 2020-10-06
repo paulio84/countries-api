a8ad59eae8d2738103e64ed685f47a872eab49cf
Paul Terry <paul.terry84@gmail.com>
2020-10-06 20:17:42 +0100

    Added BaseStyles and fonts

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
+import BaseStyles from '../styles/BaseStyles';
 import NormaliseStyles from '../styles/NormaliseStyles';
+
+import '../styles/fontStyles.css';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <NormaliseStyles />
+      <BaseStyles />
       <Component {...pageProps} />
     </>
   );
---
+++ styles/BaseStyles.js
@@ -0,0 +1,29 @@
+import { createGlobalStyle, css } from 'styled-components';
+
+export default createGlobalStyle`
+  :root {
+    --Blue: #2b3945;           /* (Dark Mode Elements) */
+    --DarkBlue: #202c37;       /* (Dark Mode Background) */
+    --VeryDarkBlue: #111517;   /* (Light Mode Text) */
+    --DarkGray: #858585;       /* (Light Mode Input) */
+    --VeryLightGray: #fafafa;  /* (Light Mode Background) */
+    --White: #ffffff;          /* (Dark Mode Text & Light Mode Elements) */
+  }
+
+  *, *::before, *::after {
+    box-sizing: inherit;
+  }
+
+  html {
+    box-sizing: border-box;
+    font-size: 62.5%;
+  }
+
+  body {
+    background-color: var(--DarkBlue);
+    color: var(--White);
+    font-family: 'NunitoSansSemiBold';
+    font-size: 1.6rem;
+    font-weight: normal;
+  }
+`;
