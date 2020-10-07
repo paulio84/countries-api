ba5bf632cead351efb127bc7a2d52fe87043c24e
Paul Terry <paul.terry84@gmail.com>
2020-10-07 22:04:15 +0100

    updated query params when getting all countries

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import CountryList from '../components/CountryList';
 import Layout from '../components/Layout';
 
 export default function Index({ countries }) {
+  console.log(countries);
   return (
     <Layout>
       <CountryList countries={countries} />
@@ -10,7 +11,9 @@ export default function Index({ countries }) {
 }
 
 export async function getStaticProps() {
-  const response = await fetch('https://restcountries.eu/rest/v2/all');
+  const response = await fetch(
+    'https://restcountries.eu/rest/v2/all?fields=alpha3Code;name;population;region;capital;flag'
+  );
   const countries = await response.json();
 
   return {
