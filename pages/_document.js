0da61d3076af8f5237d5a8a9c6b74b357386e8bc
Paul Terry <paul.terry84@gmail.com>
2020-10-14 09:30:12 +0100

    use getInitialProps instead (#2)

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
-  static async getServerSideProps(context) {
+  static async getInitialProps(context) {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = context.renderPage;
 
@@ -12,7 +12,7 @@ export default class MyDocument extends Document {
           enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />)
         });
 
-      const initialProps = await Document.getServerSideProps(context);
+      const initialProps = await Document.getInitialProps(context);
       return {
         ...initialProps,
         styles: (
